7416fd0a07d05b08a74128193f51ab10f64130cd
Emad Rahman <emadpoke@gmail.com>
2020-05-24 16:08:54 -0700

    Initial Commit

---
+++ PomPom TImer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PomPom TImer
+//
+//  Created by Emad Rahman on 2020-05-24.
+//  Copyright © 2020 Emad Rahman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

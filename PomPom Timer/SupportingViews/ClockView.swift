26bf867a65e33a36380b6d7aba44de4da43b115c
Emad Rahman <emadpoke@gmail.com>
2020-05-25 11:23:44 -0700

    Added color assets and made timer use color assets instead of hardcoded colors

---
+++ PomPom Timer/SupportingViews/ClockView.swift
@@ -0,0 +1,48 @@
+//
+//  ClockView.swift
+//  PomPom TImer
+//
+//  Created by Emad Rahman on 2020-05-24.
+//  Copyright © 2020 Emad Rahman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ClockView: View {
+    var body: some View {
+        //        if UITraitCollection.current.userInterfaceStyle == .dark {
+        //            print("Dark mode")
+        //        }
+        //        else {
+        //            print("Light mode")
+        //        }
+        
+        
+        ZStack {
+            ZStack {
+                Circle()
+                    .trim(from: 0, to: 1)
+                    .stroke(Color("TimerCircleBackground"), style: StrokeStyle(lineWidth: 35, lineCap: .round))
+                    .frame(width: 280, height: 280)
+                Circle()
+                    .trim(from: 0, to: 0.5)
+                    .stroke(Color("TimerCircleForeground"), style: StrokeStyle(lineWidth: 35, lineCap: .round))
+                    .frame(width: 280, height: 280)
+            }
+            .rotationEffect(.init(degrees: -90))
+            
+            Text("1:00")
+                .font(.largeTitle)
+                .fontWeight(.black)
+        }
+    }
+    
+}
+
+struct ClockView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ClockView()
+        }
+    }
+}

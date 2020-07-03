d0d674b5d6bf36a7cd675ca842036e127e9f8b98
Emad Rahman <emadpoke@gmail.com>
2020-07-02 18:50:32 -0700

    updated styling of buttons

--- PomPom Timer/TimerViews/ButtonsView.swift
+++ PomPom Timer/TimerViews/ButtonsView.swift
@@ -10,36 +10,60 @@ import SwiftUI
 
 struct ButtonsView: View {
     @EnvironmentObject var timerState: TimerState
+    var buttonWidth: CGFloat = 90
+    var buttonHeight: CGFloat = 40
     
     var body: some View {
         HStack {
             Spacer()
             
-            Button(action: {
-                self.timerState.started.toggle()
-            }) {
-                if !self.timerState.started {
-                    Image(systemName: "play.circle.fill")
-                        .imageScale(.large)
-                    Text("Start")
-                    .fixedSize()
-                }
-                else{
-                    Image(systemName: "pause.circle.fill")
-                        .imageScale(.large)
-                    Text("Stop")
+            ZStack{
+                Rectangle()
+                    .frame(width: buttonWidth, height: buttonHeight)
+                    .foregroundColor(.green)
+                    .cornerRadius(10)
+                
+                Button(action: {
+                    self.timerState.started.toggle()
+                }) {
+                    if !self.timerState.started {
+                        HStack{
+                            Image(systemName: "play.circle.fill")
+                                .imageScale(.large)
+                                .foregroundColor(.white)
+                            Text("Start")
+                                .fixedSize()
+                                .foregroundColor(.white)
+                        }
+                    }
+                    else{
+                        Image(systemName: "pause.circle.fill")
+                            .imageScale(.large)
+                            .foregroundColor(.white)
+                        Text("Stop")
+                            .foregroundColor(.white)
+                    }
                 }
             }
                 
             Spacer()
             
             if !self.timerState.started {
-                Button(action: {
-                    self.timerState.reset()
-                }) {
-                    Image(systemName: "arrow.uturn.left.circle.fill")
-                        .imageScale(.large)
-                    Text("Reset")
+                ZStack{
+                    Rectangle()
+                    .frame(width: buttonWidth, height: buttonHeight)
+                    .foregroundColor(.gray)
+                    .cornerRadius(10)
+                    
+                    Button(action: {
+                        self.timerState.reset()
+                    }) {
+                        Image(systemName: "arrow.uturn.left.circle.fill")
+                            .imageScale(.large)
+                            .foregroundColor(.white)
+                        Text("Reset")
+                            .foregroundColor(.white)
+                    }
                 }
                 Spacer()
             }
